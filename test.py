ee32a47fdd49140d215c46fbcd9f9420d71ba4ed
ds-5 <52705420+ds-5@users.noreply.github.com>
2019-07-10 15:11:31 +0900

    Create test.py

---
+++ test.py
@@ -0,0 +1,36 @@
+import def_a as s1
+import def_b as s2
+import sys, filecmp
+from inspect import getmembers, isfunction
+
+def run(func, log_file):
+    o = sys.stdout
+    sys.stdout = open(log_file, 'w')
+    r = func()
+    sys.stdout = o
+    return r
+
+m1, m2 = getmembers(s1), getmembers(s2)
+if len(m1) != len(m2):
+    print('[ERROR] function count = %d, %d' % (len(m1), len(m2)))
+    sys.exit()
+
+for f1, f2 in zip(m1, m2):
+    if isfunction(f1[1]):
+        log1, log2 = '{}_ans1.txt'.format(f1[0]), '{}_ans2.txt'.format(f2[0])
+        r1 = run(f1[1], log1)
+        r2 = run(f2[1], log2)
+        if r1 != r2:
+            print('[FAIL] {}'.format(f1[0]))
+            print('>>>{}'.format(r1))
+            print('>>>{}'.format(r2))
+        print(r1,r2)
+        
+        if filecmp.cmp(log1, log2):
+            print('cmp pass!', log1, log2)
+            print('{}'.format(open(log1).read()))
+            print('{}'.format(open(log2).read()))
+        else:
+            print('[FAIL] {}'.format(f1[0]))
+            print('>>>{}'.format(open(log1).read()))
+            print('>>>{}'.format(open(log2).read()))
