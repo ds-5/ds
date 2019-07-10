feed56ad1b120344305985f8e65a6112ded76ded
ds-5 <52705420+ds-5@users.noreply.github.com>
2019-07-11 00:05:51 +0900

    Update hw4_pbh.py

--- hw4_pbh.py
+++ hw4_pbh.py
@@ -5,10 +5,12 @@ def f1(l):
             cnt += 1
     return cnt
 
+
 def f2(l):
     for n in l:
         if n % 2:
             print(n)
+
 
 def f3(l):
     sum = 0
@@ -17,6 +19,7 @@ def f3(l):
             sum += n
     return sum
 
+
 def f4(l):
     sum = 0
     for n in range(len(l)):
@@ -24,31 +27,40 @@ def f4(l):
             sum += n
     return sum
 
+
 def f5(l):
     m = []
     for n in l:
-        m.append(n*n)
+        m.append(n * n)
     return m
 
+
 def f6(l):
+    if not l:
+        return
     max_n = l[0]
     for n in l:
         if n > max_n:
             max_n = n
     return max_n
 
+
 def f7(l):
+    if not l :
+        return
     sum = 0
     for n in l:
         sum += n
     return sum / len(l)
 
-def f8(a,b,n):
-    for i in range(a,b+1):
+
+def f8(a, b, n):
+    for i in range(a, b + 1):
         if i % n == 0:
             print(i)
 
-def f9(width,height):
+
+def f9(width, height):
     for r in range(height):
         for c in range(width):
             print('*', end='')
@@ -57,9 +69,10 @@ def f9(width,height):
 
 def f10(n):
     for i in range(n):
-        for j in range(i+1):
+        for j in range(i + 1):
             print('*', end='')
         print('')
+
 
 def f11(l):
     if len(l):
@@ -69,21 +82,23 @@ def f11(l):
                 return False
             i = n
     return True
-        
+
 
 def f12(l):
-    if len(l) :
+    if len(l):
         for n in l:
             if n >= 0:
                 return False
     return True
 
-def f13(l,t):
+
+def f13(l, t):
     index = 0
     for i in range(len(l)):
         if t == l[i]:
             index = i
     return index
+
 
 def f14(l):
     index = 0
@@ -92,6 +107,7 @@ def f14(l):
             index = i
     return index
 
+
 def f15(l):
     sum_var = 0
     for i in range(len(l)):
@@ -99,23 +115,26 @@ def f15(l):
             sum_var += l[i]
     return sum_var
 
+
 def f16(n):
     for i in range(n):
-        print('*'*(n-i))
+        print('*' * (n - i))
 
 
 def f17(l):
-    if len(l) :
-        for i in range(len(l)-1,-1,-2) :
-            if i >= 0 :
+    if len(l):
+        for i in range(len(l) - 1, -1, -2):
+            if i >= 0:
                 print(l[i])
+
 
 def f18(n):
     v = 1
     if n >= 2:
-        for i in range(n) :
-            v *= (i+1)
+        for i in range(n):
+            v *= (i + 1)
     return v
+
 
 def f19(l):
     for n in l:
@@ -125,26 +144,29 @@ def f19(l):
                 v *= (i + 1)
         print(v)
 
+
 def f20(l):
     for n in l:
         vl = []
-        for i in range(n+1):
-            vl.append(n-i)
+        for i in range(n + 1):
+            vl.append(n - i)
         print(*vl)
 
 
 def f21(l1, l2):
     l = []
     for i in range(len(l1)):
-        l.append(l1[i]+l2[i])
+        l.append(l1[i] + l2[i])
     return l
 
+
 def f22(n):
-    for i in range(1,n+1):
+    for i in range(1, n + 1):
         if i % 2 == 0 or i % 3 == 0:
             print(i)
 
-def f23(l) :
+
+def f23(l):
     mx = l[0][0]
     for i in l:
         for j in i:
@@ -152,22 +174,25 @@ def f23(l) :
                 mx = j
     return mx
 
-def f24(l) :
-    if l[0] > l[1] :
+
+def f24(l):
+    if l[0] > l[1]:
         mx_1, mx_2 = l[0], l[1]
-    else :
+    else:
         mx_1, mx_2 = l[1], l[0]
     for n in l[2:]:
-        if n > mx_1 :
+        if n > mx_1:
             mx_1, mx_2 = n, mx_1
-        elif n > mx_2 :
+        elif n > mx_2:
             mx_2 = n
     return mx_2
+
 
 def f25(n):
     while (n // 10):
         n //= 10
     return n
+
 
 def f26(l):
     for i in l:
@@ -176,11 +201,3 @@ def f26(l):
             if j > mx:
                 mx = j
         print(mx)
-
-
-
-
-
-
-
-
