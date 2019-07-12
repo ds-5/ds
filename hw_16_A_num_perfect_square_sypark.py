d2db262252ed28e1e1f3398efc46d309f13bc39d
white0714 <52724738+white0714@users.noreply.github.com>
2019-07-12 12:20:00 +0900

    Delete hw_16_A_num_perfect_square_sypark.py

--- hw_16_A_num_perfect_square_sypark.py
+++
@@ -1,7 +0,0 @@
-def num_perfect_squares(numbers):
-    n_p_s = 0 
-    for i in range(len(numbers)):
-        if numbers[i] >= 0 :
-            if ((numbers[i] ** 0.5)%1 == 0) :
-                n_p_s += 1
-    return n_p_s
