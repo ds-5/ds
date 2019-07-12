52d8d8ab4e6f130f3c352683043f826d367f15d4
white0714 <52724738+white0714@users.noreply.github.com>
2019-07-12 12:20:08 +0900

    Delete hw_16_A_first_perfect_square_sypark.py

--- hw_16_A_first_perfect_square_sypark.py
+++
@@ -1,8 +0,0 @@
-def first_perfect_square(numbers):
-    f_p_s = -1 
-    for i in range(len(numbers)):
-        if numbers[i] >= 0 :
-            if ((numbers[i] ** 0.5)%1 == 0) :
-                f_p_s = i
-                break
-    return f_p_s
