bcff57a3d37bf7f1a7d82f8769a65219a4fa37c9
ds-5 <52705420+ds-5@users.noreply.github.com>
2019-07-13 13:27:01 +0900

    Update hw16-a_pbh.py

--- hw16-a_pbh.py
+++ hw16-a_pbh.py
@@ -1,4 +1,4 @@
-
+## NO.1
 def first_perfect_square(l):
     for i in range(len(l)):
         if l[i] >= 0:
@@ -6,6 +6,7 @@ def first_perfect_square(l):
                 return i
     return -1
 
+## NO.2
 def num_perfect_squares(l):
     cnt = 0
     for i in l:
@@ -14,6 +15,7 @@ def num_perfect_squares(l):
                 cnt += 1
     return cnt
 
+## NO.3
 def second_largest(l):
     mx_i = 0
     for i in range(len(l)):
@@ -26,3 +28,38 @@ def second_largest(l):
             mx_v = v
     return mx_v
 
+## NO.4
+def print_french(lo, hi):
+    for i in range(lo,hi+1):
+        print(i, num_in_french(i))
+    return None
+
+def num_in_french(num): #assumes 0 <= num <= 100
+    ones_list = ["zero", "un", "deux", "trois", "quatre", "cinq", "six",
+                 "sept", "huit", "neuf", "dix", "onze", "douze",
+                 "treize", "quatorze", "quinze", "seize", "dix-sept",
+                 "dix-huit", "dix-neuf"]    # 0~19
+    tens_list = ["", "dix", "vingt", "trente", "quarante", "cinquante",
+                 "soixante", "soixante", "quatre-vingt","quatre-vingt"] # 00~90
+# EXCEPTION
+    if num == 100:
+        return 'cent'
+# INIT
+    system, sep = 20, ''
+    tens, ones = '', ''
+    if 20 < num < 60:
+        system = 10
+# TENS
+    if num >= system:
+        tens = tens_list[num//10]
+    if num == 80:
+        tens += 's'
+# ONES
+    if num == 0 or num%system != 0:
+        ones = ones_list[num%system]
+# SEPERATOR
+    if tens and num < 80 and num % 10 == 1:
+        sep = ' et '
+    elif tens and num % system:
+        sep = '-'
+    return tens + sep + ones
