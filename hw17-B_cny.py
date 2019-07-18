24ecb507ac174cfe8526c830232b7cfe4ef42fcf
ChoNamYoung <52739361+ChoNamYoung@users.noreply.github.com>
2019-07-18 17:15:51 +0900

    Add files via upload

--- hw17-B_cny.py
+++ hw17-B_cny.py
@@ -116,11 +116,11 @@ if example_print_flag:
 # Function [7/7]
 
 # make pizza.py in same folder
-# def make_pizza( size, *toppings ):
-#    """Summarize the pizza we are about to make."""
-#    print( '\nMaking a ' + str( size ) + '-inch pizza with the following toppings:' )
-#    for topping in toppings:
-#           print( '- ' + topping )
+def make_pizza( size, *toppings ):
+    """Summarize the pizza we are about to make."""
+    print( '\nMaking a ' + str( size ) + '-inch pizza with the following toppings:' )
+    for topping in toppings:
+           print( '- ' + topping )
 
 if example_print_flag:
            print( '\nFunction [7/7]' )
@@ -144,6 +144,7 @@ class Dog():
         print( self.name.title() + ' rolled over!' )
 
 if example_print_flag:
+    print( '\nClass Code [1/7]' )    
     my_dog = Dog( 'willie', 6 )
     your_dog = Dog( 'lucy', 3 )
     print( "My dog's name is " + my_dog.name.title() + '.' )
@@ -154,9 +155,126 @@ if example_print_flag:
     print( 'My dog is ' + str( your_dog.age ) + ' years old.' )
     your_dog.sit()
     
+# Class Code [2/7]
 
+# make car.py file with below Car class in same folder.
+"""A class that can be used to represent a car."""
+class Car():
+    """A simple attempt to represent a car."""
+    def __init__( self, manufacturer, model, year ):
+        """Initialize attributes to describe a car."""
+        self.manufacturer = manufacturer
+        self.model = model
+        self.year = year
+        self.odometer_reading = 0
+    def get_descriptive_name( self ):
+        """Return a neatly formatted descriptive name."""
+        long_name = str( self.year ) + ' ' + self.manufacturer + ' ' + self.model
+        return long_name.title()
+    def read_odometer( self ):
+        """Print a statement showing the car's mileage."""
+        print( "This car has " + str( self.odometer_reading ) + ' miles on it.' )
+    def update_odometer( self, mileage ):
+        """
+        Set the odometor reading to the given value.
+        Reject the change if it attempts to roll the odometer back.
+        """
+        if mileage >= self.odometer_reading:
+            self.odometer_reading = mileage
+        else:
+            print( 'You can\' roll back an odometer' )
+    def increment_odometer( self, miles ):
+        """Add the given amount to the odometer reading."""
+        self.odometer_reading += miles
 
+ # Class Code [3/7]  
 
+if example_print_flag:
+    print( '\nClass Code [3/7]' )
+    my_used_car = Car( 'subaru', 'outback', 2013 )
+    print( my_used_car.get_descriptive_name() )
+    my_used_car.update_odometer( 23500 )
+    my_used_car.read_odometer()
+    my_used_car.increment_odometer( 100 )
+    my_used_car.read_odometer()
+
+# Class Code [4/7]
+
+"""A set of classes that can be used to represent electric cars."""
+
+from car import Car
+class Battery():
+    """A simple attempt to model a battery for an electric car."""
+    def __init__( self, battery_size = 60 ):
+        """Initialize the battery's attributes."""
+        self.battery_size = battery_size
+    def describe_battery( self ):
+        """Print a statement describing the battery size."""
+        print( 'This car has a ' + str( self.battery_size ) + '-kWh battery.' )
+    def get_range( self ):
+        """Print a statement about the range this battery provides."""
+        if self.battery_size == 60:
+            range = 140
+        elif self.battery_size == 85:
+            range = 185
+        message = 'This car can go approximately ' + str( range )
+        message += ' miles on a full charge.'
+        print( message )
+
+# Make electric_car.py file with below ElectricCar class in same folder.
+from car import Car
+from battery import Battery
+
+class ElectricCar( Car ):
+    """Models aspects of a car, specific to electric vehicles."""
+    def __init__( self, manufacturer, model, year ):
+        """
+        Initialize attributes of the parent class.
+        Then initialize attributes specific to an electric car.
+        """
+        super().__init__( manufacturer, model, year )
+        self.battery = Battery()
+
+# Class Code [5/7]
+
+if example_print_flag:
+    print( '\nClass Code [5/7]' )
+    my_tesla = ElectricCar( 'tesla', 'model s', 2016 )
+    print( my_tesla.get_descriptive_name() )
+    my_tesla.battery.describe_battery()
+
+# Class Code [6/7]
+
+from car import Car
+from electric_car import ElectricCar
+
+if example_print_flag:
+    print( '\nClass Code [6/7]' )
+    my_new_car = Car( 'audi', 'a4', 2015 )
+    print( my_new_car.get_descriptive_name() )
+    my_new_car.odometer_reading = 23
+    my_new_car.read_odometer()
+    my_beetle = Car( 'volkswagen', 'beetle', 2015 )
+    print( my_beetle.get_descriptive_name() )
+    my_tesla = ElectricCar( 'tesla', 'roadster', 2015 )
+    print( my_tesla.get_descriptive_name() )
+
+# Class Code [7/7]
+
+from collections import OrderedDict
+
+favorite_languages = OrderedDict()
+favorite_languages[ 'jen' ] = 'python'
+favorite_languages[ 'sarah' ] = 'c'
+favorite_languages[ 'edward' ] = 'ruby'
+favorite_languages[ 'phil' ] = 'python'
+
+if example_print_flag:
+    print( '\nClass Code [7/7]' )
+    for name, language in favorite_languages.items():
+        print( name.title() + '\'s favorite language is ' + language.title() + '.' )
+                    
+    
 
 
 
