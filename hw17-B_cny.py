23de1c496eb812d52eb3589c77f0e379ed5440c1
ChoNamYoung <52739361+ChoNamYoung@users.noreply.github.com>
2019-07-18 16:09:31 +0900

    Update hw17-B_cny.py

--- hw17-B_cny.py
+++ hw17-B_cny.py
@@ -128,8 +128,32 @@ if example_print_flag:
            p.make_pizza( 16, 'pepperoni' )
            p.make_pizza( 12, 'mushrooms', 'green peppers', 'extra cheese' )
 
+# Class Code [1/7]
 
+class Dog():
+    """A simple attempt to model a dog."""
+    def __init__( self, name, age ):
+        """Initialize name and age attributes."""
+        self.name = name
+        self.age = age
+    def sit( self ):
+        """Simulate a dog sitting in response to a command."""
+        print( self.name.title() + ' is now sitting.' )
+    def roll_over( self ):
+        """Simulate rolling over in response to a command."""
+        print( self.name.title() + ' rolled over!' )
 
+if example_print_flag:
+    my_dog = Dog( 'willie', 6 )
+    your_dog = Dog( 'lucy', 3 )
+    print( "My dog's name is " + my_dog.name.title() + '.' )
+    print( 'My dog is ' + str( my_dog.age ) + ' years old.' )
+    my_dog.sit()
+
+    print( "\nMy dog's name is " + your_dog.name.title() + '.' )
+    print( 'My dog is ' + str( your_dog.age ) + ' years old.' )
+    your_dog.sit()
+    
 
 
 
