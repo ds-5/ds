35901bf88e5d192955dacf6b3639a8fcf6d1bd81
white0714 <52724738+white0714@users.noreply.github.com>
2019-08-03 17:35:15 +0900

    Update hw_30_A_sypark.py

--- hw_30_A_sypark.py
+++ hw_30_A_sypark.py
@@ -2,23 +2,28 @@ import numpy as np
 
 #(1) Create an array of size 10 filled with 0
 x=np.zeros(10, dtype='i')
+print(x)
 
 #(2) Set fifth value to 1 from above array
 x[4] = 1
+print(x)
 
 #(3) Create an array with values ranging from 10 to 49
 x=np.arange(10,50)
+print(x)
 
 #(4) Create a 5 * 5 matrix with values ranging from 0 to 24
 x=np.arange(25).reshape((5,5))
+print(x)
 
 #(5) Create a 5*5 identity matrix
 x=np.eye(5, dtype='i')
+print(x)
 
 #(6) Create an 5*5 array with random values and find the minimum and maximum value
 x=np.random.rand(5,5)
-min_x = x.min()
-max_x = x.max()
+print(x.min())
+print(x.max())
 
 '''
 (7) Multiply a 4*3 matrix by a 3*2 matrix
@@ -28,9 +33,10 @@ max_x = x.max()
 A=np.ones((4,3), dtype='i')
 B=np.random.rand(3,2)
 Mul_mat = A@B
+print(Mul_mat)
 
 #(8) Transpose the above matrix
-Mul_mat.T
+print(Mul_mat.T)
 
 '''
 (9) Create two matrices ranging from 0 to 24 and 25 to 49
@@ -41,3 +47,5 @@ A=np.arange(25).reshape(5,5)
 B=np.arange(25, 50).reshape(5,5)
 Add_mat = A+B
 Sub_mat = A-B
+print(Add_mat)
+print(Sub_mat)
