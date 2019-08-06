8d0a05b6bcef105addb18399eec595450118d3d4
ChoNamYoung <52739361+ChoNamYoung@users.noreply.github.com>
2019-08-06 20:24:42 +0900

    Update hw25-A_cny.py

--- hw25-A_cny.py
+++ hw25-A_cny.py
@@ -1,4 +1,4 @@
-# Drawing Unix History
+# 25-A-1 : Drawing Unix History
 
 from graphviz import Digraph
 engines = ['dot', 'neato', 'fdp', 'sfdp', 'twopi', 'circo']
@@ -12,4 +12,40 @@ for engine in engines:
     g.edges([('4.2 BSD','4.3 BSD'), ('4.2 BSD','Ultrix-32')])
     g.render('./25_A_1_'+engine, view=True)
 
-# Drawing Red-Black Tree as example
+# 25-A-2 : Drawing Red-Black Tree as example
+
+from graphviz import Digraph
+g = Digraph('Red-Black Tree', engine = 'dot')
+g.attr('graph', ration='0.8')
+
+#NIL 
+g.attr('node', style='filled', fillcolor='black', fontcolor='white', shape='record', width='0.4', label='NIL', height='0.25')
+nil_nodes = ['1L', '6L', '6R', '11L', '11R', '15L', '15R', '22L', '22R', '27L', '27R']
+for node in nil_nodes:
+    g.node( node, label='NIL')
+
+#Black Dot
+g.attr('node', style='filled', fillcolor='black', fontcolor='white', shape='circle', width='0.6')
+black_nodes = ['13', '1', '11', '15', '25']
+for node in black_nodes:
+    g.node( node, label=node)
+
+#Red Dot
+g.attr('node', style='filled', fillcolor='red', fontcolor='white', shape='circle', width='0.6')
+red_nodes = ['8','17','6','22','27']
+for node in red_nodes:
+    g.node( node, label=node)
+
+#edges
+g.edges([('13',  '8'), ( '13', '17'), 
+         ( '8',  '1'), (  '8', '11'),
+         ('17', '15'), ( '17', '25'),
+         ( '1', '1L'), (  '1',  '6'),
+         ( '6', '6L'), (  '6', '6R'),
+         ('11','11L'), ( '11','11R'),
+         ('15','15L'), ( '15','15R'),
+         ('25', '22'), ( '25', '27'),
+         ('22','22L'), ( '22','22R'),
+         ('27','27L'), ( '27','27R')])
+
+g.render('./25_A_2_Red-Black_Tree', view=True)
