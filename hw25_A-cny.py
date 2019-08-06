56293fee0305f978f1ec69401e8fc398c634cb7f
ChoNamYoung <52739361+ChoNamYoung@users.noreply.github.com>
2019-08-06 19:29:09 +0900

    Update and rename hw25_A_cny.py to hw25_A-cny.py

--- hw25_A-cny.py
+++ hw25_A-cny.py
@@ -1,3 +1,5 @@
+# Drawing Unix History
+
 from graphviz import Digraph
 engines = ['dot', 'neato', 'fdp', 'sfdp', 'twopi', 'circo']
 for engine in engines:
@@ -9,3 +11,5 @@ for engine in engines:
     g.edges([('32V','3 BSD'),('3 BSD', '4 BSD'), ('4 BSD','4.1 BSD'), ('4.1 BSD','4.2 BSD'), ('4.1 BSD','2.8 BSD'), ('4.1 BSD', '8th Edition')])
     g.edges([('4.2 BSD','4.3 BSD'), ('4.2 BSD','Ultrix-32')])
     g.render('./25_A_1_'+engine, view=True)
+
+# Drawing Red-Black Tree as example
