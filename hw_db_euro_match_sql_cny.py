90d48205fcb4440edf2c5d56ce3658e44648ab45
ChoNamYoung <52739361+ChoNamYoung@users.noreply.github.com>
2019-08-07 02:54:33 +0900

    Update hw_db_euro_match_sql_cny.py

--- hw_db_euro_match_sql_cny.py
+++ hw_db_euro_match_sql_cny.py
@@ -12,5 +12,18 @@ df_clubs = sql.read_sql_query("""
 df_clubs10 = df_clubgoals.head(10)
 
 # Column 이름을 수정한다.
-df_clubs10.rename( columns = { "playing_club":"club_name", "count(*)":"goals" })
+df_club10 = df_clubs10.rename( columns = { "playing_club":"club_name", "count(*)":"goals" })
 
+# Top 10 클럽의 순위를 Bar chart로 표시한다.
+import matplotlib.pyplot as plt
+
+position = list( df_clubs10.index )
+goals = []
+for i in range( 10 ):
+    goals.append( df_club10.goals[i])
+
+# Y축은 Club name, X축은 클럽 소속 선수들의 총 득점 수
+plt.barh( position, goals, align = 'center', alpha=1)
+plt.yticks( position, df_clubs10.playing_club )
+plt.xlabel( 'Goal score')
+plt.title( 'Top 10 club in goal score')
