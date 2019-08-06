91b44c275328f9893eaa1cb13f9f9628564d2334
ChoNamYoung <52739361+ChoNamYoung@users.noreply.github.com>
2019-08-06 19:27:11 +0900

    Create hw25_A_cny.py

---
+++ hw25_A_cny.py
@@ -0,0 +1,11 @@
+from graphviz import Digraph
+engines = ['dot', 'neato', 'fdp', 'sfdp', 'twopi', 'circo']
+for engine in engines:
+    g = Digraph('G', engine=engine)
+    g.node_attr.update(style='filled', color='goldenrod2', width='0.75')
+    g.edges([('7th Edition', '32V'),('7th Edition', 'V7M'),('7th Edition', 'Xenix'),('7th Edition', 'UniPlus+')])
+    g.edges([('8th Edition', '9th Edition')])
+    g.edges([('1 BSD','2 BSD'),('2 BSD','2.8 BSD'), ('2.8 BSD','Ultrix-11'), ('2.8 BSD','2.9 BSD')])
+    g.edges([('32V','3 BSD'),('3 BSD', '4 BSD'), ('4 BSD','4.1 BSD'), ('4.1 BSD','4.2 BSD'), ('4.1 BSD','2.8 BSD'), ('4.1 BSD', '8th Edition')])
+    g.edges([('4.2 BSD','4.3 BSD'), ('4.2 BSD','Ultrix-32')])
+    g.render('./25_A_1_'+engine, view=True)
