304879bc9745d6a8a7fd7b0c8f4cc8d18741c816
ChoNamYoung <52739361+ChoNamYoung@users.noreply.github.com>
2019-08-10 14:32:16 +0900

    Create hw35-A_cny.py

---
+++ hw35-A_cny.py
@@ -0,0 +1,10 @@
+%matplotlib inline
+import warnings
+warnings.filterwarnings("ignore")
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn import datasets, preprocessing
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
